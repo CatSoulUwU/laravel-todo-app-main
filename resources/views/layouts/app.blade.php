2b21555bd1205e8f075b6eb0b319e2813f7856d4
CatSoulUwU <68296562+CatSoulUwU@users.noreply.github.com>
2022-06-13 20:21:49 +0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,18 @@
     <link rel="stylesheet" href="{{ secure_asset("css/main.css") }}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+    .navbar-brand-logo {
+    min-height: auto;
+    min-width: 50%;}
+    </style>
 </head>
 <body>
     <div id="app">
         <nav class="nav navbar navbar-expand-md navbar-light cool">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{secure_asset('assets/logo.svg')}}" alt="logo">
+                    <img class="navbar-brand-logo" src="{{secure_asset('assets/logo.svg')}}" alt="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
